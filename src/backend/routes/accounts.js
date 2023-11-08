cd558e8a852f2107690dcf3c9cfa58047cbb5f09
Chris Wood <wchristopherwood912@gmail.com>
2023-11-08 08:33:49 -0800

    add RUD of CRUD

--- src/backend/routes/accounts.js
+++ src/backend/routes/accounts.js
@@ -1,11 +1,14 @@
 import Express from "express";
-import { getUsers } from '../Controllers/users/profile.js';
+import { deleteUser, getUserById, getUsers, updateUser } from '../Controllers/users/profile.js';
 
 const router = Express.Router();
 
 router.use("/accounts", router);
 
-//get all users
-router.get("/users", getUsers)
+//User CRUD routes
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.put("/update/:id", updateUser);
+router.delete("/delete:id", deleteUser);
 
 export default router;
