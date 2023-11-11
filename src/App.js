aeea4e1a341c5d9828bd847c2f845d361a03af91
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-11-10 18:14:08 -0800

    added about section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './styles/App.css';
 import NavBar from './components/Navbar.jsx';
 import Landing from './components/Landing.jsx';
 import Content from './components/Content.jsx';
+import About from './components/About.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Landing />
       <Content />
+      <About />
     </div>
   );
 }
