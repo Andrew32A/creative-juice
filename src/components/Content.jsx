524aa94419a05de7cc1b268dcf3eb2d421db82cc
Andrew Alsing <andrew32alsing@gmail.com>
2023-11-19 20:46:24 -0800

    Updated content component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -17,18 +17,27 @@ const Content = () => {
       delay: 400
     });    
   }, []);
-  
+
   return (
-    <div name='home' className="w-full h-screen bg-[#F2E2C4]">
-    <div className='grid-wrapper' style={{ display: 'flex', justifyContent: 'center'}}>
-      <div style={{ marginRight: '10px' }}>
-        <img className="w-[250px] mx-auto" src={Placeholder} alt="placeholder" aria-label="placeholder image" />
-      </div>
-      <div style={{ marginLeft: '10px' }}>
-        <img className="w-[250px] mx-auto" src={Placeholder} alt="placeholder" aria-label="placeholder image" />
+  <div className="text-center mt-8 mb-3" style={{ fontFamily: 'Mogra, sans-serif'}}>
+    <h1 className="text-5xl mb-8">Artwork Shared:</h1>
+    <div className="m-10 shadow-md rounded-md p-10">
+      <div className="grid-wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center">
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 1" />
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 2" />
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 3" />
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 4" />
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 5" />
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 6" />
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 7" />
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 8" />
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 9" />
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 10" />
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 11" />
+        <img className="w-full h-64 object-cover rounded-md" src={Placeholder} alt="Artwork 12" />
       </div>
     </div>
-    </div>
+  </div>
   );
 }
 
