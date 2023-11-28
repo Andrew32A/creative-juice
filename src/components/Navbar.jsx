f0327fbd9be2d955cebedca84d61e1cd4f8452c1
Andrew Alsing <andrew32alsing@gmail.com>
2023-11-28 12:01:24 -0800

    Hid prompt and profile for users not logged in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,14 +39,14 @@ const NavBar = () => {
 
   useEffect(() => {
     const sr = ScrollReveal({
-      distance: "60px",
+      distance: '60px',
       reset: false,
     });
 
     sr.reveal('.nav-wrapper', {
       origin: 'top',
       interval: 100,
-      delay: 500
+      delay: 500,
     });
   }, []);
 
@@ -58,35 +58,58 @@ const NavBar = () => {
 
   return (
     <div style={{ fontFamily: 'DM Sans, sans-serif' }}>
-      <div className={`z-50 fixed w-full flex justify-between items-center px-4 transition-all duration-500 ease-in-out ${isScrolled ? 'bg-opacity-80 backdrop-blur-sm shadow-lg' : 'bg-[#F2E2C4]'} text-[#fcf8c1] ${hideHeader ? '-top-32' : 'top-0'}`}>
+      <div
+        className={`z-50 fixed w-full flex justify-between items-center px-4 transition-all duration-500 ease-in-out ${
+          isScrolled
+            ? 'bg-opacity-80 backdrop-blur-sm shadow-lg'
+            : 'bg-[#F2E2C4]'
+        } text-[#fcf8c1] ${hideHeader ? '-top-32' : 'top-0'}`}
+      >
         <Link className="flex items-center" to="/">
           <img src={Logo} alt="" style={{ width: '50px' }} />
         </Link>
 
         <ul className="nav-wrapper hidden md:flex space-x-4 text-pink-300">
           <li style={{ display: 'flex', alignItems: 'center' }}>
-            <Link to="/" className="nav-link text-black border-[#003000] border-2 px-4 py-1 inline-flex items-center justify-center bg-[#BBF2E2] hover:bg-[#F2A7CA] transition duration-300 ease-in-out">
+            <Link
+              to="/"
+              className="nav-link text-black border-[#003000] border-2 px-4 py-1 inline-flex items-center justify-center bg-[#BBF2E2] hover:bg-[#F2A7CA] transition duration-300 ease-in-out"
+            >
               <i className="ri-home-8-line"></i>
               <span className="pl-2">Home</span>
             </Link>
 
-            <Link to="/prompt" className="nav-link text-black border-[#003000] border-2 px-4 py-1 ml-[-2px] inline-flex items-center justify-center bg-[#BBF2E2] hover:bg-[#F2A7CA] transition duration-300 ease-in-out">
+            <Link
+              to="/prompt"
+              className="nav-link text-black border-[#003000] border-2 px-4 py-1 ml-[-2px] inline-flex items-center justify-center bg-[#BBF2E2] hover-bg-[#F2A7CA] transition duration-300 ease-in-out"
+            >
               <i className="ri-pencil-line"></i>
               <span className="pl-2">Prompt</span>
             </Link>
 
-            <Link to="/profile" className="nav-link text-black border-[#003000] border-2 px-4 py-1 ml-[-2px] inline-flex items-center justify-center bg-[#BBF2E2] hover:bg-[#F2A7CA] transition duration-300 ease-in-out">
-              <i className="ri-profile-line"></i>
-              <span className="pl-2">Profile</span>
-            </Link>
+            {isAuthenticated && (
+              <Link
+                to="/profile"
+                className="nav-link text-black border-[#003000] border-2 px-4 py-1 ml-[-2px] inline-flex items-center justify-center bg-[#BBF2E2] hover:bg-[#F2A7CA] transition duration-300 ease-in-out"
+              >
+                <i className="ri-profile-line"></i>
+                <span className="pl-2">Profile</span>
+              </Link>
+            )}
 
             {!isAuthenticated ? (
-              <Link to="/login" className="nav-link text-black border-[#003000] border-2 px-4 py-1 ml-[-2px] inline-flex items-center justify-center bg-[#BBF2E2] hover:bg-[#F2A7CA] transition duration-300 ease-in-out">
+              <Link
+                to="/login"
+                className="nav-link text-black border-[#003000] border-2 px-4 py-1 ml-[-2px] inline-flex items-center justify-center bg-[#BBF2E2] hover:bg-[#F2A7CA] transition duration-300 ease-in-out"
+              >
                 <i className="ri-login-box-line"></i>
                 <span className="pl-2">Login/Sign Up</span>
               </Link>
             ) : (
-              <button onClick={handleLogout} className="nav-link text-black border-[#003000] border-2 px-4 py-1 ml-[-2px] inline-flex items-center justify-center bg-[#BBF2E2] hover:bg-[#F2A7CA] transition duration-300 ease-in-out">
+              <button
+                onClick={handleLogout}
+                className="nav-link text-black border-[#003000] border-2 px-4 py-1 ml-[-2px] inline-flex items-center justify-center bg-[#BBF2E2] hover:bg-[#F2A7CA] transition duration-300 ease-in-out"
+              >
                 <i className="ri-logout-box-line"></i>
                 <span className="pl-2">Logout</span>
               </button>
@@ -96,6 +119,6 @@ const NavBar = () => {
       </div>
     </div>
   );
-}
+};
 
 export default NavBar;
