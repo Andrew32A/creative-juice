4c2f68c012624f3e6fa76725ee87b7d9043b7be0
Chris Wood <wchristopherwood912@gmail.com>
2023-12-04 17:27:01 -0800

    add prompt upload script

---
+++ src/backend/utils/promptUpload.js
@@ -0,0 +1,32 @@
+import {readFile} from 'fs/promises';
+const url = 'http://localhost:3000/api/prompts/new';
+
+const prompts = JSON.parse(await readFile('./src/backend/utils/prompts.json', 'utf8'));
+
+const uploadPrompts = async () => {
+  for(let i = 0; i < prompts.length; i++) {
+    const prompt = prompts[i]
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(prompt),
+        });
+
+        if (!response.ok) {
+            throw new Error('Error uploading prompts');
+        }
+
+        const data = await response.json();
+        console.log('Prompts uploaded:', data);
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+  }
+    
+};
+
+
+uploadPrompts();
